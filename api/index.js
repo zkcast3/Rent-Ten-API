306a33c089ec32ae95279ecf27d203a8cb8585ef
zkcast3 <125942241+zkcast3@users.noreply.github.com>
2023-02-21 12:57:29 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const { conn } = require("./src/db.js");
 const axios = require("axios");
 const server = require("./src/app.js");
 
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(process.env.PORT, () => {
     console.log("%s listening at 3001"); // eslint-disable-line no-console
     // axios.post("http://localhost:3001/houses/fulldb");
